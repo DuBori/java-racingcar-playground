37c8222a791a6e1d01ced2c15df4d7e8b375d789
DuBori <92709439+DuBori@users.noreply.github.com>
2023-08-03 23:06:34 +0900

    기능 요구사항 2번 TDD 완료

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,11 +1,22 @@
 public class Car {
     private String name;
-
+    private StringBuffer stack = new StringBuffer();
     public Car(String name) {
         this.name = name;
+    }
+    public void go() {
+        stack.append("-");
     }
 
     public String getName() {
         return name;
+    }
+
+    public StringBuffer getStack() {
+        return stack;
+    }
+
+    public String print() {
+        return name + ":" + getStack();
     }
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -5,21 +5,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class CarTest {
 
     @Test
-    void 자동차이름부여테스트_5보다작을때(){
+    void 자동차이름부여테스트_5보다작을때() {
         assertTrue(new Car("빵빵이").getName().length() < 5);
     }
 
     @Test
-    void 자동차이름부여테스트_5보다클때(){
+    void 자동차이름부여테스트_5보다클때() {
         assertFalse(new Car("빵빵이빵빵빵").getName().length() < 5);
     }
 
     @Test
-    void 자동차이름_5자초과했을때기능(){
+    void 자동차이름_5자초과했을때기능() {
         assertThrows(NumberFormatException.class,() -> {
             CarUtills.validate(new Car("빵빵이빵빵빵").getName());
         });
     }
-
+    @Test
+    void 자동차_전진기능_테스트() {
+        Car 빵빵이 = new Car("빵빵이");
+        빵빵이.go();
+        System.out.println(빵빵이.print());
+    }
     
 }
